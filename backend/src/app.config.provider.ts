23c9674edbf8162f14dff2be27c3da34363280db
Andrey Krivosheev <andreykrt12@gmail.com>
2026-05-01 14:11:05 +0300

    remove mongo

--- backend/src/app.config.provider.ts
+++ backend/src/app.config.provider.ts
@@ -3,15 +3,24 @@ import { ConfigService } from '@nestjs/config';
 export const CONFIG = 'CONFIG';
 
 export interface AppConfig {
-  database: { driver: string; url: string };
+  database: {
+    url: string;
+    port: number;
+    username: string;
+    password: string;
+    dbName: string;
+  };
 }
 
 export const configProvider = {
   provide: CONFIG,
   useFactory: (cfg: ConfigService): AppConfig => ({
     database: {
-      driver: cfg.get<string>('DATABASE_DRIVER', 'mongodb'),
       url: cfg.get<string>('DATABASE_URL')!,
+      port: cfg.get<number>('DATABASE_PORT'),
+      username: cfg.get<string>('DATABASE_USER'),
+      password: cfg.get<string>('DATABASE_PASSWORD'),
+      dbName: cfg.get<string>('DATABASE_NAME'),
     },
   }),
   inject: [ConfigService],
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,49 +1,46 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
-import { configProvider } from './app.config.provider';
+import { AppConfig, CONFIG, configProvider } from './app.config.provider';
 import { FilmsController } from './films/films.controller';
 import { FilmsService } from './films/films.service';
 import { join } from 'path';
 import { OrdersController } from './orders/orders.controller';
 import { FilmRepository } from './films/domain/ports/film.repository.port';
-// import { FilmRepositoryMongo } from './films/infrastructure/mongodb/film.repository.mongo';
-import { Film, FilmSchema } from './films/infrastructure/mongodb/film.schema';
+
 import { OrderRepository } from './orders/domain/ports/order.repository.port';
 import { OrderService } from './orders/orders.service';
-// import { OrderRepositoryMongo } from './orders/infrastructure/mongodb/order.repository.mongo';
-
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SessionEntity } from './films/infrastructure/postgres/entities/session.entity';
 import { Tag } from './films/infrastructure/postgres/entities/tag.entity';
-import { MongooseModule } from '@nestjs/mongoose';
-import {
-  Order,
-  OrderSchema,
-} from './orders/infrastructure/mongodb/order.schema';
 import { FilmEntity } from './films/infrastructure/postgres/entities/film.entity';
 import { FilmRepositoryPostgres } from './films/infrastructure/postgres/film.repository.postgres';
 import { OrderRepositoryPostgres } from './orders/infrastructure/postgres/order.repository.postgres';
 import { TicketEntity } from './orders/infrastructure/postgres/entities/ticket.entity';
 import { OrderEntity } from './orders/infrastructure/postgres/entities/order.entity';
+import { AppConfigModule } from './app.config.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, cache: true }),
+    AppConfigModule,
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public', 'content', 'afisha'),
       serveRoot: '/content/afisha',
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'nest_project',
-      entities: [FilmEntity, SessionEntity, Tag, TicketEntity, OrderEntity],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [AppConfigModule],
+      inject: [CONFIG],
+      useFactory: (config: AppConfig) => ({
+        type: 'postgres',
+        host: config.database.url,
+        port: config.database.port,
+        database: config.database.dbName,
+        username: config.database.username,
+        password: config.database.password,
+        entities: [FilmEntity, SessionEntity, Tag, TicketEntity, OrderEntity],
+      }),
     }),
     TypeOrmModule.forFeature([
       FilmEntity,
@@ -51,16 +48,6 @@ import { OrderEntity } from './orders/infrastructure/postgres/entities/order.ent
       Tag,
       OrderEntity,
       TicketEntity,
-    ]),
-    MongooseModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (cfg: ConfigService) => ({
-        uri: cfg.get<string>('DATABASE_URL', 'mongodb://localhost:27017/prac'),
-      }),
-    }),
-    MongooseModule.forFeature([
-      { name: Film.name, schema: FilmSchema },
-      { name: Order.name, schema: OrderSchema },
     ]),
   ],
   controllers: [FilmsController, OrdersController],
